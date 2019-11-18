194baa842d20b35abb478bf6374130e7c34f2131
Govintharaj <53900377+Govintharaj@users.noreply.github.com>
2019-11-18 10:48:23 +0530

    Add files via upload

---
+++ github.py
@@ -0,0 +1,43 @@
+ git branch govi
+ 1295  git init
+ 1296  git branch govi
+ 1297  git branch master
+ 1298  git checkout
+ 1299  git add .
+ 1300  git status
+ 1301  git branch govi
+ 1302  git push https://github.com/Govintharaj/Django_sample.git
+ 1303  git push --set-upstream https://github.com/Govintharaj/Django_sample.git master
+ 1304  git push --set-upstream https://github.com/Govintharaj/Django_sample.git
+ 1305  git push --set-upstream https://github.com/Govintharaj/Django_sample.git master
+ 1306  git pull
+ 1307  git pull https://github.com/Govintharaj/Django_sample.git
+ 1308  git push --set-upstream https://github.com/Govintharaj/Django_sample.git master
+ 1309  git push
+ 1310  git branch govi
+ 1311  git checkout goiv
+ 1312  git checkout govi
+ 1313  git push
+ 1314  git push https://github.com/Govintharaj/Django_sample.git
+ 1315  git push --set-upstream https://github.com/Govintharaj/Django_sample.git govi
+ 1316  git push
+ 1317  git commit
+ 1318  got config --global user.email govikingston@gmail.com
+ 1319  git config --global user.email govikingston@gmail.com
+ 1320  git config --global user.name govi
+ 1321  git commit
+ 1322  git push
+
+
+
+Commit in Git:
+…or create a new repository on the command line
+
+echo "# sample" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Govintharaj/sample.git
+git push -u origin master
+git remote add origin https://github.com/Govintharaj/sample.git
+git push -u origin master
